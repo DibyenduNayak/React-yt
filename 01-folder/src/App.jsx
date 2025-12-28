8b558c17aef2679a2589933c068f9c6a32801526
DibyenduNayak <dibyendunayak120@gmail.com>
2025-12-28 15:58:57 +0530

    folder added

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,21 @@
+
+const App = () => {
+  return (
+    <>
+    
+     <div id= 'parent'>
+    <h1 id="chield1">  to kes ho app log</h1>
+    <h2 id="chiield2">hello</h2>
+
+     </div>
+         <div id="chacha"><h1>hello bachooo</h1></div>
+
+    
+    </>
+
+
+  
+  )
+}
+
+export default App
