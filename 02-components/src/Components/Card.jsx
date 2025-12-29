2ee7da0b4a6786e59385bf25aae041cc667f30bb
DibyenduNayak <dibyendunayak120@gmail.com>
2025-12-30 00:48:11 +0530

    components folder

---
+++ 02-components/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <div className='card'>
+        <h1>Dibyendu Nayak</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus voluptas pariatur quam at error fugiat odio doloremque corporis eaque, quia non ducimus cumque quidem iusto id voluptatum fugit eligendi!</p>
+      
+    </div>
+    </div>
+  )
+}
+
+export default card
